58ee223e051061eeba333888d25f7b86358e49cc
Jin Woong Kim <johnjinkim1@gmail.com>
2016-12-24 19:20:50 -0800

    Update fblogin.py
    removed sensitive data

--- fblogin.py
+++ fblogin.py
@@ -3,18 +3,11 @@ import requests
 import pyquery
 
 def login(session, email, password):
-    '''
-    Attempt to login to Facebook. Returns user ID, xs token and
-    fb_dtsg token. All 3 are required to make requests to
-    Facebook endpoints as a logged in user. Returns False if
-    login failed.
-    '''
-
     # Navigate to Facebook's homepage to load Facebook's cookies.
-    response = session.get('https://m.facebook.com')
+    response = session.get('')
 
     # Attempt to login to Facebook
-    response = session.post('https://m.facebook.com/login.php', data={
+    response = session.post('', data={
         'email': email,
         'pass': password
     }, allow_redirects=False)
@@ -23,7 +16,7 @@ def login(session, email, password):
     if 'c_user' in response.cookies:
 
         # Make a request to homepage to get fb_dtsg token
-        homepage_resp = session.get('https://m.facebook.com/home.php')
+        homepage_resp = session.get('')
 
         dom = pyquery.PyQuery(homepage_resp.text.encode('utf8'))
         fb_dtsg = dom('input[name="fb_dtsg"]').val()
@@ -38,13 +31,13 @@ def login(session, email, password):
 
 def call_login(email, password):
 
-    parser = argparse.ArgumentParser(description='Login to Facebook')
+    parser = argparse.ArgumentParser(description='')
     parser.add_argument('email', help='Email address')
     parser.add_argument('password', help='Login password')
 
     session = requests.session()
     session.headers.update({
-        'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0'
+        ''
     })
 
     flag, res, home = login(session, email, password)
